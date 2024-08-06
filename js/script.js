3582d89babda42132b182e709864649c7d50cf40
Henrique José Dias Pereira <henriquejdpereira@gmail.com>
2024-08-05 23:48:02 -0300

    mod:general

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+const nodes = document.querySelectorAll('.tree-node > .node-link');
+
+nodes.forEach(node => {
+    node.addEventListener('click', function(event) {
+        event.preventDefault();
+        const children = this.nextElementSibling;
+
+        if (children) {
+            children.style.display = children.style.display === 'none' ? 'block' : 'none';
+        }
+    });
+});
+
 document.addEventListener('DOMContentLoaded', function() {
     const coordenadores = document.querySelectorAll('.coordenador');
     const funcionarios = document.querySelector('.funcionarios');
